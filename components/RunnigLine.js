f93b8690ac39a057e63743e1941ce6b6b0d03b49
ValNeedsPower <valera75step@gmail.com>
2022-06-04 20:19:24 +0300

    RunningLine is improved
    1) Design fixed (paddings between lines and elements)
    
    2) <marquee> tag removed
    
    3) added CSS animation

--- components/RunnigLine.js
+++ components/RunnigLine.js
@@ -2,17 +2,17 @@ const RunnigLine = () => {
   const renderCommingSoon = Array(100)
     .fill(null)
     .map((element, index) => (
-      <div className="flex p-2" key={index}>
-        <span>ART COMING SOON</span>
+      <div className="flex p-2 space-x-5" key={index}>
+        <p className="w-[200px]">ART COMING SOON</p>
         <img className="w-5 " src="/images/movingLineImg.svg" alt="element" />
       </div>
     ));
 
   return (
-    <div className="relative mt-[125px] border-t border-b border-white-1 py-6 font-Grotesk text-xl font-medium text-white-1">
-      <marquee direction="right">
-        <div className="flex gap-x-5">{renderCommingSoon}</div>
-      </marquee>
+    <div className="relative mt-[170px] border-t border-b border-white-1 py-6 font-Grotesk text-xl font-medium text-white-1">
+      
+        <div id="running" className="flex gap-x-5">{renderCommingSoon}</div>
+     
     </div>
   );
 };
