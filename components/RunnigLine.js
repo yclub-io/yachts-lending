c30226c7260faa901972573a2d21dcf24090d828
ValNeedsPower <valera75step@gmail.com>
2022-06-05 12:49:59 +0300

    Running line changed
    1) CSS styles are hidden now
    2) Added bg-color

--- components/RunnigLine.js
+++ components/RunnigLine.js
@@ -2,14 +2,14 @@ const RunnigLine = () => {
   const renderCommingSoon = Array(100)
     .fill(null)
     .map((element, index) => (
-      <div className="flex p-2 space-x-5" key={index}>
-        <p className="w-[200px]">ART COMING SOON</p>
+      <div className="flex p-2 space-x-5 " key={index}>
+        <p className="w-[200px] ">ART COMING SOON</p>
         <img className="w-5 " src="/images/movingLineImg.svg" alt="element" />
       </div>
     ));
 
   return (
-    <div className="relative mt-[170px] border-t border-b border-white-1 py-6 font-Grotesk text-xl font-medium text-white-1">
+    <div className="relative mt-[170px] border-t bg-black-1 border-b border-white-1 py-6 font-Grotesk text-xl font-medium text-white-1">
       
         <div id="running" className="flex gap-x-5 overflow-x-clip">{renderCommingSoon}</div>
      
