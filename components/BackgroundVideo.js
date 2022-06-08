4998b8950993543d9dea53dcc23481767f90053b
ValNeedsPower <valera75step@gmail.com>
2022-06-08 21:17:09 +0300

    BG video SAFARI bag attempt 4 removed

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -9,7 +9,7 @@ const BackgroundVideo = () => {
       playsinline
       disablePictureInPicture
     >
-      <source src="video/background.mp4" type="video/ogg"/>
+      <source src="video/background.mp4" type="video/mp4"/>
       Your browser does not support the video tag.
     </video>
       </div>
