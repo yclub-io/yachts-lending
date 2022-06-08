b6d65ff39ed59e26814023d86d5867e9e1661e22
IgorTimo <rostovplace@yandex.ru>
2022-06-08 16:18:09 +0300

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,32 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import '../styles/globals.css'
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    import('react-facebook-pixel')
+    import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init('5162146007173539') 
-        ReactPixel.pageView()
+        ReactPixel.init("5162146007173539");
+        ReactPixel.pageView();
 
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
 
-
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon-32x32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
