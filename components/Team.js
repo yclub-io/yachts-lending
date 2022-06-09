f65f1e8e8c588646a84997709d86106e7a6629ad
ValNeedsPower <valera75step@gmail.com>
2022-06-09 17:58:15 +0300

    Team Section design changed back
    Team section changed back to the previous version

--- components/Team.js
+++ components/Team.js
@@ -6,156 +6,117 @@ const Team = () => {
       </p>
 
         <div className="flex lg:flex-row flex-col items-center lg:gap-[15px] gap-y-[45px]">
-          
+
           {/*member 1*/}
-             
-            <div className="relative flex w-[285px] border border-black-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
-                <p className="z-10 font-bold text-[28px] pt-[50px]"> 
+            <div className="flex w-[285px] border border-white-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
+                <p className="font-bold text-[28px] pt-[50px]"> 
                   Neptune
                 </p>
-                    <p className="z-10 text-sm text-gray-1 pt-[25px]"> 
+                    <p className="text-sm text-gray-1 pt-[25px]"> 
                       Founder
                     </p>
                       
                       <img
-                        className=" z-10 w-8 mx-auto py-[20px]"
+                        className="w-8 mx-auto py-[20px]"
                         src="/images/teamSectionInfDivider.svg"
                         alt="divider"
                       ></img>
                     
-                        <p className=" z-10 w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
+                        <p className="w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
                         Basically born on a Super Yacht,
                         years of experience in helping 
                         to curate and market extravagant 
                         experiences for those chartering 
                         and owning these vessels of extravagance.
                         </p>
-                        <div className="absolute lg:left-[-5px] left-[10px] top-[0px] w-[400px]">
-                    <img
-                         className="w-[260px]"
-                         src="/images/teamSectionBorder.svg"
-                         alt="border"
-                    />  
-                  </div>
             </div>
-            
-                 
-            
 
           {/*member 2*/}
-          <div className="relative flex w-[285px] border border-black-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
-                <p className="z-10 font-bold text-[28px] pt-[50px]"> 
+          <div className="flex w-[285px] border border-white-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
+                <p className="font-bold text-[28px] pt-[50px]"> 
                   Mega Skipper
                 </p>
-                    <p className="z-10 text-sm text-gray-1 pt-[25px]"> 
+                    <p className="text-sm text-gray-1 pt-[25px]"> 
                       Campaigns
                     </p>
                       
                       <img
-                        className="z-10 w-8 mx-auto py-[20px]"
+                        className="w-8 mx-auto py-[20px]"
                         src="/images/teamSectionInfDivider.svg"
                         alt="divider"
                       ></img>
                     
-                        <p className="z-10 w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
+                        <p className="w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
                         Ex VP of digital in largest Israeli
                         advertising agency with 25 years of
                         experience in online advertising.   
                         </p>
-                        <div className="absolute lg:left-[-5px] left-[10px] top-[0px] w-[400px]">
-                    <img
-                         className="w-[260px]"
-                         src="/images/teamSectionBorder.svg"
-                         alt="border"
-                    />  
-                  </div>
             </div>
 
           {/*member 3*/}
-          <div className="relative flex w-[285px] border border-black-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
-                <p className="z-10 font-bold text-[28px] pt-[50px]"> 
+          <div className="flex w-[285px] border border-white-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
+                <p className="font-bold text-[28px] pt-[50px]"> 
                   Blade
                 </p>
-                    <p className="z-10 text-sm text-gray-1 pt-[25px]"> 
+                    <p className="text-sm text-gray-1 pt-[25px]"> 
                       Strategies
                     </p>
                       
                       <img
-                        className="z-10 w-8 mx-auto py-[20px]"
+                        className="w-8 mx-auto py-[20px]"
                         src="/images/teamSectionInfDivider.svg"
                         alt="divider"
                       ></img>
                     
-                        <p className="z-10 w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
+                        <p className="w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
                         Engineered for several Fortune 500 companies
                         in a variety of technology related roles while
                         building his website into the top 1000 most
                         visited websites in the world. 
                         </p>
-                        <div className="absolute lg:left-[-5px] left-[10px] top-[0px] w-[400px]">
-                    <img
-                         className="w-[260px]"
-                         src="/images/teamSectionBorder.svg"
-                         alt="border"
-                    />  
-                  </div>    
             </div>
 
           {/*member 4*/}
-          <div className="relative flex w-[285px] border border-black-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
-                <p className="z-10 font-bold text-[28px] pt-[50px]"> 
+          <div className="flex w-[285px] border border-white-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
+                <p className="font-bold text-[28px] pt-[50px]"> 
                   Cobra
                 </p>
-                    <p className="z-10 text-sm text-gray-1 pt-[25px]"> 
+                    <p className="text-sm text-gray-1 pt-[25px]"> 
                       Marketing
                     </p>
                       
                       <img
-                        className="z-10 w-8 mx-auto py-[20px]"
+                        className="w-8 mx-auto py-[20px]"
                         src="/images/teamSectionInfDivider.svg"
                         alt="divider"
                       ></img>
                     
-                        <p className="z-10 w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
+                        <p className="w-[245px] mx-auto text-base text-gray-1 leading-[20px] pb-[60px]"> 
                         10 years of experience helping E-commerce brands
                         launch and scale while generating over a billion
                         dollars in sales for some of the world's retail giants. 
                         </p>
-                        <div className="absolute lg:left-[-5px] left-[10px] top-[0px] w-[400px]">
-                    <img
-                         className="w-[260px]"
-                         src="/images/teamSectionBorder.svg"
-                         alt="border"
-                    />  
-                  </div>
             </div>
 
           {/*member 5*/}
-          <div className="relative flex w-[285px] border border-black-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
-                <p className="z-10 font-bold text-[28px] pt-[50px]"> 
+          <div className="flex w-[285px] border border-white-1 rounded-[20px] basis-1/5 flex-col font-Grotesk text-center h-[355px] px-1"> 
+                <p className="font-bold text-[28px] pt-[50px]"> 
                   Poseidon
                 </p>
-                    <p className="z-10 text-sm text-gray-1 pt-[25px]"> 
+                    <p className="text-sm text-gray-1 pt-[25px]"> 
                       Operations
                     </p>
                       
                       <img
-                        className="z-10 w-8 mx-auto py-[20px]"
+                        className="w-8 mx-auto py-[20px]"
                         src="/images/teamSectionInfDivider.svg"
                         alt="divider"
                       ></img>
                     
-                        <p className="z-10 w-[245px] mx-auto text-base text-gray-1 leading-[20px] lg:pb-0 pb-[60px]"> 
+                        <p className="w-[245px] mx-auto text-base text-gray-1 leading-[20px] lg:pb-0 pb-[60px]"> 
                         Chief Operations Officer in many successful start ups.
                         I always say there is no bad business there is a bad operation. 
                         </p>
-                        <div className="absolute lg:left-[-5px] left-[10px] top-[0px] w-[400px]">
-                    <img
-                         className="w-[260px]"
-                         src="/images/teamSectionBorder.svg"
-                         alt="border"
-                    />  
-                  </div>
             </div>
 
   
