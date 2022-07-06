5c46ac69acaed91802a30cfb7703da77afa26bf9
ValNeedsPower <valera75step@gmail.com>
2022-07-07 00:05:27 +0300

    Team changes

--- components/Team.js
+++ components/Team.js
@@ -62,7 +62,7 @@ const teamData = [
 const Team = () => {
   return (
     <div id="team" className=" text-white-1 lg:mx-14">
-      <p className="mb-[68px] flex justify-center text-center font-Plus text-[26px] font-extrabold lg:mb-[125px] lg:text-5xl">
+      <p className="mb-[68px] mt-[215px] flex justify-center text-center font-Plus text-[26px] font-extrabold lg:mb-[125px] lg:text-5xl">
         Team
       </p>
       <TeamDesktop teamData={teamData} />
