4389ec7a1bcb7a3ce8e0d40b023e8234130e2f8b
ValNeedsPower <valera75step@gmail.com>
2022-07-07 00:05:18 +0300

    Footer changes

--- components/Hero.js
+++ components/Hero.js
@@ -3,38 +3,40 @@ import Button from "../TW_components /Button";
 const Hero = () => {
   return (
     <>
-      <div id='hero' className="relative">
-      </div>
-      <div className="flex lg:flex-row  flex-col lg:px-14 px-4 pt-9 font-Grotesk text-white-1">
-        <div className="z-10 flex basis-1/2 flex-col">
-          <p className="lg:text-[180px] text-[100px] font-bold md:text-center">YCLUB</p>
-          <span className="lg:text-base text-xs md:text-center font-normal lg:pr-[450px]">NFT PROJECT</span>
+      <div id='hero' className="mx-8 relative flex mt-[824px]  tracking-[-2%] font-Grotesk text-white-1">
+        {/*DIV TEXT*/}
+        <div className="flex basis-1/2 flex-col text-left mt-[-70px]">
+          <h1 className="font-bold text-[180px]"> 
+            YCLUB
+          </h1>
+            <h2 className="mt-[-20px] text-[40px] font-bold"> 
+              JULY 31 &#160;
+              <span className="text-white-1 bg-gradient-to-b from-purple-2 via-purple-3 to-purple-2 bg-clip-text text-opacity-[0%]">
+               2022
+              </span>
+            </h2>
+            <h2 className="flex mt-[256px] font-bold text-[64px] leading-[81.66px]"> 
+             JOIN &#160;
+              <a className="flex"> 
+                <img src="./images/HeroSection/discord_big_logo.svg"
+                     className="mr-[15px]"/> 
+                <span className="text-white-1 bg-gradient-to-r from-blue-2 to-pink-2 bg-clip-text text-opacity-[0%]">
+                DISCORD 
+                </span>
+              </a>
+            </h2>
+            <h2 className="flex font-bold text-[64px] leading-[81.66px]"> 
+              FOR WHITELIST INFO
+            </h2>
+            
         </div>
-        <div className="z-10 flex lg:basis-1/2 flex-col lg:pl-20">
-          <p className="flex flex-col lg:pt-[50px] pt-[60px] lg:text-[180px] text-[100px] font-bold lg:leading-[180px] leading-[100px] md:text-center">
-            JULY
-            <span className=" bg-gradient-to-t from-blue-1 to-purple-1 bg-clip-text text-black-1 text-opacity-0 md:text-center">
-              2022
-            </span>
-          </p>
-          <p className="lg:w-[400px] min-w-[319px] lg:pt-8 pt-4 lg:text-lg text-base lg:ml-[80px] lg:text-left md:text-center text-left">
-            Your charter into a world of extravagance both in the Metaverse and
-            beyond.
-          </p>
+        {/*DIV GIF*/}
+        <div className="flex basis-1/2">
+          <img src="./images/gif_test.png"
+              className="max-w-[660px] max-h-[660px]"/>
         </div>
-      </div>
       
-      <a href="#whitelist"> 
-      <div className="flex justify-center lg:mt-[85px] mt-[70px]"> 
-      <button
-       className="lg:w-auto w-[342px]  absolute z-40 text-white-1 border border-pink-1 bg-black-1 shadow-btnGlowInit py-3 px-12 rounded-full font-Grotesk text-base 
-        hover:shadow-btnGlowHover font-medium tracking-wider transition-all duration-700 hover:py-4 hover:px-14"> 
-
-    APPLY FOR WHITELIST
-    </button>
       </div>
-      </a>
-      
     </>
   );
 };
