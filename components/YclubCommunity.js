6b190c65d6ca1d1aa3fe188f8ae84cd5fc4b2474
ValNeedsPower <valera75step@gmail.com>
2022-07-07 00:06:05 +0300

    YClubCommunity changed

--- components/YclubCommunity.js
+++ components/YclubCommunity.js
@@ -1,36 +1,51 @@
 const YclubCommunity = () => {
   return (
-    <div className="lg:mx-14 mx-4 lg:mt-[900px] mt-[100px] flex flex-col text-white-1">
-      <p className=" z-40 lg:mb-[45px] mb-[35px] font-Plus lg:text-8xl text-4xl lg:font-semibold font-extrabold  lg:text-left text-center">
+    <div className="lg:mx-14 mx-4 mt-[200px] flex flex-col text-white-1">
+      <p className="lg:mb-[45px] mb-[35px] font-Plus lg:text-[96px] text-4xl lg:font-semibold font-extrabold  lg:text-left text-center">
         YCLUB Community
       </p>
 
     {/*Desktop version*/}
-      <div className="lg:flex hidden flex-row border-t border-b">
-        <div className="flex w-[440px] flex-col pt-[60px] text-left font-Grotesk text-lg font-normal leading-[30px]">
-          <p>Follow out twitter &amp;</p>
-          <p>Join our discord</p>
-        </div>
-        <div className="flex w-[290px]  justify-center">
+      <div className="lg:flex hidden flex-col ">
+        
+        <p className="mt-[20px]"> 
+          Follow our twitter &#38; Join our discord
+        </p>
+
+        <div className="flex flex-row 
+                        mt-[64px] space-x-[100px]
+                        font-Grotesk text-4xl font-normal">
           <a href="https://twitter.com/yclub_io" target="_blank">
-            <p className="border-l border-r py-[65px] px-[88px] font-Grotesk text-4xl font-normal">
+            <p className="flex">
+              <img src="./images/YclubCommunity/share_arrow.svg"
+                   className="mr-[15px]"/>
               twitter
             </p>
           </a>
-        </div>
-        <div className="flex w-[290px]  justify-center">
           <a href="https://discord.gg/yclub" target="_blank">
-            <p className=" border-r py-[65px] px-[88px] font-Grotesk text-4xl font-normal">
+            <p className="flex">
+             <img src="./images/YclubCommunity/share_arrow.svg"
+                  className="mr-[15px]"/>
               discord
             </p>
           </a>
-        </div>
-        <div className="flex w-[290px]  justify-center">
           <a href="https://t.me/yachtsnftclub" target="_blank">
-            <p className="py-[65px] ml-10 font-Grotesk text-4xl font-normal">
-              telegram
+            <p className="flex">
+              <img src="./images/YclubCommunity/share_arrow.svg"
+                   className="mr-[15px]"/>
+              instagram
             </p>
           </a>
+          <a href="https://t.me/yachtsnftclub" target="_blank">
+            <p className="flex">
+              <img src="./images/YclubCommunity/share_arrow.svg"
+                   className="mr-[15px]"/>
+              reddit
+            </p>
+          </a>
+        
+       
+         
         </div>
       </div>
 
