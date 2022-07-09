c069ca0e0d18d300a33dea5444a3a814a254bf88
ValNeedsPower <valera75step@gmail.com>
2022-07-09 11:21:30 +0300

    BG video section changed

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -1,7 +1,14 @@
 const BackgroundVideo = () => {
     return (
-        <div className="-z-20">
-    <video
+        <div className="flex justify-center relative">
+          {/*PLAY BUTTON*/}
+          <img src="./video/play-button.svg"
+             className="flex absolute z-10 left-[666px] top-[385px]"/>
+          {/*VIDEO FILE*/}
+        <img src="./video/video_screen_shot.png"
+             className="flex justify-center m-auto aspect-square absolute top-[125px] right-[0px] lg:h-[725px] lg:min-w-[1440px] object-cover"/>
+          
+    {/*<video
       className="flex opacity-50 absolute top-[0px] right-[0px] object-cover h-[920px] min-w-[2000px] w-full"
       autoPlay
       loop
@@ -11,7 +18,7 @@ const BackgroundVideo = () => {
     >
       <source src="video/background.mp4" type="video/mp4"/>
       Your browser does not support the video tag.
-    </video>
+    </video>*/}
       </div>
     );
   };
