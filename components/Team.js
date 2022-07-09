86f1234ce75c673464ebd325426a12e4140480a4
ValNeedsPower <valera75step@gmail.com>
2022-07-09 13:28:54 +0300

    Team section design changed

--- components/Team.js
+++ components/Team.js
@@ -5,7 +5,7 @@ import teamData from "../data/teamData";
 const Team = () => {
   return (
     <div id="team" className=" text-white-1 lg:mx-14">
-      <p className="mb-[68px] mt-[347px] flex justify-center text-center font-Plus font-extrabold lg:mb-[125px] lg:text-5xl text-[26px]">
+      <p className="mb-[68px] lg:mt-[347px] mt-[203px] flex justify-center text-center font-Plus font-extrabold lg:mb-[125px] lg:text-5xl text-[26px]">
         Team
       </p>
       <TeamDesktop teamData={teamData} />
