c4b4953b5b13e843b8ef45ccf81913f457cce4e7
ValNeedsPower <valera75step@gmail.com>
2022-07-09 13:28:04 +0300

    Background video mobile version added

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -1,12 +1,12 @@
 const BackgroundVideo = () => {
     return (
-        <div className="flex justify-center relative">
+        <div className="relative">
           {/*PLAY BUTTON*/}
           <img src="./video/play-button.svg"
-             className="flex absolute z-10 left-[666px] top-[385px]"/>
+             className="absolute z-10 left-0 right-0 m-auto lg:w-[98px] w-[76px] lg:top-[385px] top-[261px]"/>
           {/*VIDEO FILE*/}
         <img src="./video/video_screen_shot.png"
-             className="flex justify-center m-auto aspect-square absolute top-[125px] right-[0px] lg:h-[725px] lg:min-w-[1440px] object-cover"/>
+             className="flex justify-center m-auto aspect-square absolute lg:top-[125px] top-[109px] right-[0px] lg:h-[725px] lg:min-w-[1440px] object-cover"/>
           
     {/*<video
       className="flex opacity-50 absolute top-[0px] right-[0px] object-cover h-[920px] min-w-[2000px] w-full"
