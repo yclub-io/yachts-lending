f43acb349ecf2c893b961f62a19d55aa7b1de421
IgorTimo <rostovplace@yandex.ru>
2022-07-12 17:42:50 +0300

    changed teamData

--- data/teamData.js
+++ data/teamData.js
@@ -1,38 +1,17 @@
 const teamData = [
     {
-      name: "Neptune",
-      meta: "Founder",
-      desc: "Basically born on a Super Yacht, years of experience in helping to curate and market extravagant experiences for those chartering and owning these vessels of extravagance.",
+      name: "TerpHeadRick",
+      meta: "Community Manager",
+      desc: "Have been in crypto since 2019 and first got into Discord and community building/managing for stock signals groups. Naturally as I got more into crypto I shifted into that and haven’t looked back since."},
+    {
+      name: "The Yacht Mogul",
+      meta: "Advisor",
+      desc: "All passion and love for yachts and their ability to inspire people all around the world. A chosen “Influencial figure in the yachting world” by some of the press such as Forbes, CNN, BOAT, Robb Report.",
     },
     {
-      name: "Poseidon",
-      meta: "Operations",
-      desc: "Chief Operations Officer in many successful start ups. I always say there is no bad business there is a bad operation.",
-    },
-    {
-      name: "Mega Skipper",
-      meta: "Campaigns",
-      desc: "Ex VP of digital in largest Israeli advertising agency with 25 years of experience in online advertising.",
-    },
-    {
-      name: "Blade",
-      meta: "Strategies",
-      desc: "Engineered for several Fortune 500 companies in a variety of technology related roles while building his website into the top 1000 most visited websites in the world.",
-    },
-    {
-      name: "De Vinchi",
-      meta: "Financial Analyst",
-      desc: "has over 10 years of consulting and building working relationships between customers and their brand partners. Through years of experience in banking, and financial services, David’s managed to institute financial systems to strengthen portfolio health.",
-    },
-    {
-      name: "Cobra",
-      meta: "Marketing",
-      desc: "20 years of experience helping E-commerce brands launch and scale while generating over a billion dollars in sales for some of the world's retail giants.",
-    },
-    {
-      name: "Lipek",
-      meta: "Community manager",
-      desc: "10 years of experience in growing communities on social media. Since 2012 grew over 15m followers and reached over 300m users.",
+      name: "ValTron",
+      meta: "Creative Producer",
+      desc: "Creative mastermind! She is the best there is in our eyes and we love what she does. She was the screen writer for FlixPatrol that reached the TOP 10 movies on Netflix in the World in 2021.",
     },
     {
       name: "Carrara",
@@ -45,14 +24,39 @@ const teamData = [
       desc: "Photographer and Strategist, with over 5 years of helping brands strengthen their communications, communities, and lifestyle experiences.",
     },
     {
-      name: "Yacht Mogul",
-      meta: "Advisor",
-      desc: "All passion and love for yachts and their ability to inspire people all around the world. A chosen “Influencial figure in the yachting world” by some of the press such as Forbes,CNN,BOAT,Robb Report.",
+      name: "Mega Skipper",
+      meta: "Campaign Manager",
+      desc: "Ex VP of digital in largest Israeli advertising agency with 25 years of experience in online advertising.",
+    },
+    {
+      name: "Poseidon",
+      meta: "Co-Founder / COO",
+      desc: "10 years of experience in growing communities on social media. Since 2012 grew over 15m followers and reached over 300m users.",
     },
     {
       name: "CryptoDon",
-      meta: "Advisor",
+      meta: "Advisor / Influencer",
       desc: "Early investor in many projects like BAYC and in the ethereum blockchain. Dedicated curator in owned Web3 investment communities.",
+    },
+    {
+      name: "Neptune",
+      meta: "Co-Founder",
+      desc: "Basically born on a Super Yacht, years of experience in helping to curate and market extravagant experiences for those chartering these vessels of extravagance.",
+    },
+    {
+      name: "Cobra",
+      meta: "Co-Founder / Strategy",
+      desc: "22 years of online marketing experience helping brands launch and scale while generating over a billion dollars in sales for some of the world's retail giants.",
+    },
+    {
+      name: "Blade",
+      meta: "Strategy",
+      desc: "Engineered for several Fortune 500 companies in a variety of technology related roles while building his website into the top 1000 most visited websites in the world.",
+    },
+    {
+      name: "De Vinchi",
+      meta: "Co-Founder / Financial Analyst",
+      desc: "Over 10 years of consulting & building working relationships between customers and their brand partners. Through years of experience in banking, and financial services, managed to institute financial systems to strengthen portfolio health.",
     },
   ];
 
