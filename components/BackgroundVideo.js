526ff0c7f4ac6a68f90ea855b6e546f138c2c807
ValNeedsPower <valera75step@gmail.com>
2022-07-12 11:44:27 +0300

    BG image changed

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -1,12 +1,12 @@
 const BackgroundVideo = () => {
     return (
-        <div className="relative">
+        <div className="relative flex w-screen m-auto">
           {/*PLAY BUTTON*/}
           <img src="./video/play-button.svg"
-             className="absolute z-10 left-0 right-0 m-auto lg:w-[98px] w-[76px] lg:top-[280px] top-[261px]"/>
+             className="hidden absolute z-10 left-0 right-0 m-auto lg:w-[98px] w-[76px] lg:top-[280px] top-[261px]"/>
           {/*VIDEO FILE*/}
-        <img src="./video/video_screen_shot.png"
-             className="flex justify-center m-auto aspect-square lg:h-[725px] lg:min-w-[1440px] object-cover"/>
+        <img src="./images/Background/hero_picture.png"
+             className="aspect-square lg:h-[725px] lg:min-w-[1440px]  object-cover"/>
           
     {/*<video
       className="flex opacity-50 absolute top-[0px] right-[0px] object-cover h-[920px] min-w-[2000px] w-full"
