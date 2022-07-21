c293cd155801a219f52c4c3190bebe063959f0f1
IgorTimo <rostovplace@yandex.ru>
2022-07-21 18:43:27 +0300

    added play button

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -6,7 +6,7 @@ const BackgroundVideo = () => {
   const desc = `<iframe src="https://player.vimeo.com/video/731504910?h=5b573395d0" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`;
   const desc2 = `<iframe src="https://player.vimeo.com/video/731504910?h=da13c4bfd8&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="width:100%;height:100%;" title="YCLUB.mov"></iframe>`;
   return (
-    <div className="relative m-auto flex w-screen">
+    <div className=" m-auto flex w-screen">
       {/*VIDEO FILE*/}
 
       {isPlaying ? (
@@ -21,7 +21,12 @@ const BackgroundVideo = () => {
           />
         </>
       ) : (
-        <>
+        <div className="relative">
+          <img
+            src="./video/play-button.svg"
+            className="absolute top-1/2 left-1/2 w-10 cursor-pointer"
+            onClick={() => setPlaing(true)}
+          />
           <img
             src="./images/Background/hero_picture.png"
             className="hidden aspect-square object-cover lg:block lg:h-[725px]  lg:min-w-[1440px]"
@@ -30,7 +35,7 @@ const BackgroundVideo = () => {
             src="./images/Background/hero_picture_mobile.jpg"
             className=" m-auto aspect-square w-full lg:hidden"
           />
-        </>
+        </div>
       )}
 
       {/*<video
