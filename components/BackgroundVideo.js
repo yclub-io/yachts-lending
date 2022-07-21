9ab96964f51a78a4a6aa121120d15d79a95a5195
IgorTimo <rostovplace@yandex.ru>
2022-07-21 18:32:00 +0300

    added video

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -1,15 +1,37 @@
+import { useState } from "react";
+
 const BackgroundVideo = () => {
+  const [isPlaying, setPlaing] = useState(false);
+
+  const desc = `<iframe src="https://player.vimeo.com/video/731504910?h=5b573395d0" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`;
+  const desc2 = `<iframe src="https://player.vimeo.com/video/731504910?h=da13c4bfd8&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="width:100%;height:100%;" title="YCLUB.mov"></iframe>`;
   return (
     <div className="relative m-auto flex w-screen">
       {/*VIDEO FILE*/}
-      <img
-        src="./images/Background/hero_picture.png"
-        className="hidden aspect-square object-cover lg:block lg:h-[725px]  lg:min-w-[1440px]"
-      />
-      <img
-        src="./images/Background/hero_picture_mobile.png"
-        className=" m-auto aspect-square w-full lg:hidden "
-      />
+
+      {isPlaying ? (
+        <>
+          <div
+            className="hidden aspect-square object-cover lg:block lg:h-[900px]  lg:min-w-[1440px]"
+            dangerouslySetInnerHTML={{ __html: desc2 }}
+          />
+          <div
+            className=" m-auto aspect-square w-full lg:hidden"
+            dangerouslySetInnerHTML={{ __html: desc2 }}
+          />
+        </>
+      ) : (
+        <>
+          <img
+            src="./images/Background/hero_picture.png"
+            className="hidden aspect-square object-cover lg:block lg:h-[725px]  lg:min-w-[1440px]"
+          />
+          <img
+            src="./images/Background/hero_picture_mobile.jpg"
+            className=" m-auto aspect-square w-full lg:hidden"
+          />
+        </>
+      )}
 
       {/*<video
       className="flex opacity-50 absolute top-[0px] right-[0px] object-cover h-[920px] min-w-[2000px] w-full"
