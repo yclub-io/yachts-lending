ca84869bee23984031fe3c50a4876e1618bf5b20
IgorTimo <rostovplace@yandex.ru>
2022-07-28 15:20:49 +0300

    made new mobile team section

--- components/Team.js
+++ components/Team.js
@@ -5,7 +5,7 @@ import teamData from "../data/teamData";
 const Team = () => {
   return (
     <div id="team" className=" text-white-1 lg:mx-14">
-      <p className="mb-[68px] lg:mt-[347px] md:mt-[5em] mt-[1em] flex justify-center text-center font-Plus font-extrabold lg:mb-[125px] lg:text-5xl text-[26px]">
+      <p className="mb-[68px] lg:mt-[147px] md:mt-[5em] mt-[1em] flex justify-center text-center font-Plus font-extrabold lg:mb-[125px] lg:text-5xl text-[26px]">
         Team
       </p>
       <TeamDesktop teamData={teamData} />
--- components/TeamMobile.js
+++ components/TeamMobile.js
@@ -19,7 +19,7 @@ const TeamMobile = ({ teamData }) => {
 
   return (
     <div className="lg:hidden">
-      <div>
+      {/* <div>
         <div className="m-auto flex h-[355px] w-[285px] flex-col rounded-[20px] border border-purple-1 px-1 text-center font-Grotesk">
           <p className="pt-[50px] text-[28px] font-bold">
             {teamData[currentCardIndex].name}
@@ -38,9 +38,74 @@ const TeamMobile = ({ teamData }) => {
             {teamData[currentCardIndex].desc}
           </p>
         </div>
-      </div>
+      </div> */}
+      {teamData[currentCardIndex].fullName ? (
+        <div
+          className="mx-auto flex h-[559px] w-[337px] flex-col justify-between rounded-[20px] border border-purple-1 p-5 text-center font-Grotesk"
+        >
+          <div>
+            <div className="relative w-full">
+              {teamData[currentCardIndex].socialNetworkLink && (
+                <a href={teamData[currentCardIndex].socialNetworkLink} target="_blank">
+                  <img
+                    className="absolute top-0 right-0 w-6 hover:opacity-70"
+                    src={teamData[currentCardIndex].socialNetworkImageSrc}
+                    alt="social network"
+                  />
+                </a>
+              )}
+              <img
+                className="m-auto h-[147px] w-[147px] rounded-full border border-purple-1"
+                src={teamData[currentCardIndex].avatarSrc}
+              />
+            </div>
+
+            <p className="mt-[20px] text-[24px] font-bold">{teamData[currentCardIndex].fullName}</p>
+            <p className="mt-[5px] text-[18px] font-bold">{teamData[currentCardIndex].nickName}</p>
+            <p className="mt-[10px] text-sm text-gray-1">{teamData[currentCardIndex].role}</p>
+
+            <img
+              className="mx-auto w-8 py-[10px]"
+              src="/images/teamSectionInfDivider.svg"
+              alt="divider"
+            />
+
+            <p className="w mx-auto text-base  text-gray-1 lg:w-[245px]">
+              {teamData[currentCardIndex].desc}
+            </p>
+          </div>
+          <div className="flex justify-center">
+            {teamData[currentCardIndex].pastProjectsLogosArr &&
+              teamData[currentCardIndex].pastProjectsLogosArr.map((logoSrc) => (
+                <img
+                  key={logoSrc}
+                  className="mx-[10px] h-[50px] w-[50px]"
+                  src={logoSrc}
+                  alt={logoSrc}
+                />
+              ))}
+          </div>
+        </div>
+      ) : (
+        <div
+          className="mx-auto flex h-[559px] w-[337px] flex-col rounded-[20px] border border-purple-1 px-1 text-center font-Grotesk"
+        >
+          <p className="mt-[140px] text-[28px] font-bold">{teamData[currentCardIndex].nickName}</p>
+          <p className="pt-[10px] text-sm text-gray-1">{teamData[currentCardIndex].role}</p>
+
+          <img
+            className="mx-auto w-8 py-[20px]"
+            src="/images/teamSectionInfDivider.svg"
+            alt="divider"
+          ></img>
+
+          <p className="w mx-auto pb-[60px] text-base leading-[20px] text-gray-1 lg:w-[245px]">
+            {teamData[currentCardIndex].desc}
+          </p>
+        </div>
+      )}
       <div className="flex flex-col lg:hidden">
-        <div className="mt-[55px] flex justify-center ">
+        <div className="mt-[20px] flex justify-center ">
           <img
             className=""
             src="/images/teamArrowScrollToLeft.svg"
