fd28a4d8546a333efff4525d9f69ad0c95fcaddb
ValNeedsPower <valera75step@gmail.com>
2022-08-12 12:18:51 +0300

    BG video pt added

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -4,11 +4,11 @@ const BackgroundVideo = () => {
 
   const desc = `<iframe src="https://player.vimeo.com/video/731504910?h=da13c4bfd8&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&autoplay=1&loop=1&autopause=0&muted=1:" allow="autoplay frameborder="0"; fullscreen; picture-in-picture" allowfullscreen style="width:100%;height:100%;" title="YCLUB.mov"></iframe>`;
   return (
-    <div className=" m-auto flex w-screen">
+    <div className="lg:pt-0 pt-[25px] m-auto flex w-screen">
       {/*VIDEO FILE*/}
 
       <div
-        className="hidden aspect-square object-cover lg:block lg:h-[900px]  lg:min-w-[1440px]"
+        className="hidden aspect-square object-cover  lg:block lg:h-[900px]  lg:min-w-[1440px]"
         dangerouslySetInnerHTML={{ __html: desc }}
       />
       <div
