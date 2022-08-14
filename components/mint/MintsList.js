05fa89b7f4c4abd2b8b67e33085f8b648b37f640
ValNeedsPower <valera75step@gmail.com>
2022-08-14 15:54:48 +0300

    Mobile version added to MintList section

--- components/mint/MintsList.js
+++ components/mint/MintsList.js
@@ -7,7 +7,7 @@ const MintsList = ({ currentDate, currentMintContractIndex }) => {
       key={index}
       className={`flex flex-col
     rounded-[10px] border
-   py-[20px] px-[28px] font-Grotesk text-white-1 ${
+      lg:pt-[20px] lg:pb-[20px] pt-[20px] pb-[15px] lg:pl-[20px] lg:pr-[28px] pl-[10px] pr-[16px] font-Grotesk text-white-1 ${
      currentMintContractIndex === index
        ? "border-pink-1 shadow-mintGlow"
        : "border-white-1"
@@ -19,12 +19,15 @@ const MintsList = ({ currentDate, currentMintContractIndex }) => {
         </div>
         <div>
           {currentDate < contract.start ? (
+            <div className="flex lg:flex-row flex-col">
             <p className=" text-[13px] text-pink-1">
               Starts in:
-              <span className="pl-[21px] text-white-1">
+            </p>
+              <span className="text-[13px] lg:ml-[21px] w-[107px] text-white-1">
                 {secondsToTime(contract.start - currentDate)}
               </span>
-            </p>
+            
+            </div>
           ) : currentDate < contract.end ? (
             <p className="text-[13px] text-pink-1">
               Ends in:
@@ -36,16 +39,18 @@ const MintsList = ({ currentDate, currentMintContractIndex }) => {
         </div>
       </div>
 
-      <div className="flex space-x-[15px]">
-        <div className="rounded-[16.5px] bg-white-1 bg-opacity-[20%] px-[10px] py-[5px] text-[12px] font-medium tracking-[10%]">
+      <div className="flex lg:flex-row flex-col lg:space-x-[15px]">
+        <div className="flex lg:space-x-[10px] space-x-[10px]">
+        <span className="rounded-[16.5px]  bg-white-1 bg-opacity-[20%] px-[10px] py-[5px] text-[11px] font-medium tracking-[10%]">
           {contract?.whitelist
             ? `WHITELIST ${contract.whitelist}`
             : "PUBLIC MINT"}
-        </div>
-        <div className="rounded-[16.5px] bg-white-1 bg-opacity-[20%] px-[10px] py-[5px] text-[12px] font-medium tracking-[10%]">
+        </span>
+        <span className="rounded-[16.5px] bg-white-1 bg-opacity-[20%] px-[10px] py-[5px] text-[11px] font-medium tracking-[10%]">
           {`MAX ${contract.max} PER`}
-        </div>
-        <div className="rounded-[16.5px] bg-white-1 bg-opacity-[20%] px-[10px] py-[5px] text-[12px] font-medium tracking-[10%]">
+        </span>
+        </div> 
+        <div className="lg:mt-0 mt-[5px] rounded-[16.5px] bg-white-1 bg-opacity-[20%] px-[10px] max-w-[111px] py-[5px] text-[11px] font-medium tracking-[10%]">
           {`PRICE ${contract.price} ETH`}
         </div>
       </div>
@@ -53,7 +58,7 @@ const MintsList = ({ currentDate, currentMintContractIndex }) => {
   ));
 
   return (
-    <div className="mx-[60px] mb-[56px] space-y-[20px]">{renderMints}</div>
+    <div className="lg:mx-[60px] mx-[16px] mb-[56px] space-y-[20px]">{renderMints}</div>
   );
 };
 
