d24b17086a1408877e71d88bd0d4f3f42f9a6a50
ValNeedsPower <valera75step@gmail.com>
2022-08-14 15:55:08 +0300

    Mobile version added to MintTeam section

--- components/mint/TeamInMint.js
+++ components/mint/TeamInMint.js
@@ -2,7 +2,7 @@ import teamData from "../../data/teamData";
 
 const TeamInMint = () => {
   const renderTeam = teamData.map((member, index) => (
-    <div key={index} className="m-auto mx-[60px] mb-[50px] font-Grotesk  text-white-1">
+    <div key={index} className="m-auto lg:mx-[60px] mx-[16px] mb-[50px] font-Grotesk  text-white-1">
       <p className="pt-[20px] text-[24px] font-bold ">{member.nickName}</p>
       <p className="pt-[10px] text-sm font-normal mb-[38px] text-gray-1">{member.role}</p>
       <p className="w mx-auto text-base font-normal leading-[20px] text-gray-1">{member.desc}</p>
