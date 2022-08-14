525a28472c682abafe8ac42f7048e4b7e08c1575
ValNeedsPower <valera75step@gmail.com>
2022-08-14 18:41:14 +0300

    Progress bar changed for mobile version

--- components/mint/CurrentMintProgressBar.js
+++ components/mint/CurrentMintProgressBar.js
@@ -7,7 +7,7 @@ const CurrentMintProgressBar = ({ contractInfo }) => {
   return (
     contractInfo && (
       <>
-        <div className="mt-[30px] h-[6px] w-[660px] rounded-[6px] bg-gray-1 ">
+        <div className="mt-[30px] h-[6px] lg:mx-0 mx-[16px] lg:w-[660px] rounded-[6px] bg-gray-1 ">
           <div
             style={{
               width: progress + "%",
@@ -15,7 +15,7 @@ const CurrentMintProgressBar = ({ contractInfo }) => {
             className={`z-10 h-full rounded-[6px] bg-pink-1 `}
           />
         </div>
-        <div className="mt-2 flex w-[660px]  justify-between">
+        <div className="lg:mt-[14px] mt-[18px] flex lg:w-[660px]  lg:mx-0 mx-[16px]   justify-between">
           <span className="text-sm font-light text-white-1">Total Minted</span>
           <span className="text-sm font-light text-white-1">
             <span className="font-semibold">{Math.floor(progress)}% </span>
